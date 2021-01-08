ae7120ff0a8be432fb1a6cd3196df9aa001dda22
LoneRifle <LoneRifle@users.noreply.github.com>
2021-01-08 09:35:29 +0800

    fix(prod): ensure built client served, TypeScript output correct

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,8 +6,8 @@ import config from './config'
 const app = express()
 const port = config.get('port')
 
-app.use(express.static(path.resolve(__dirname + '/../../public')))
 app.use(express.static(path.resolve(__dirname + '/../../build/client')))
+app.use(express.static(path.resolve(__dirname + '/../../public')))
 app.get('/api/hello', (_req, res) => res.send('Hello World'))
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
