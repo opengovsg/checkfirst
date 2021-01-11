1570aca1d4aa8867fc9e7492da995b51d7ba6c9f
Alexander Lee <alexander@open.gov.sg>
2021-01-11 18:30:58 +0800

    chore: update config to read css files

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,10 @@ module.exports = () => {
           },
         },
         {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
           test: /\.(png|woff|woff2|eot|ttf|svg)$/,
           use: [
             {
