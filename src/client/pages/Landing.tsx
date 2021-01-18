720931dcf171e1a33b08e8a996d2f9b54acbbb28
Lam Kee Wei <keewei.lam@gmail.com>
2021-01-18 15:31:50 +0800

    style: update style for Landing page

--- src/client/pages/Landing.tsx
+++ src/client/pages/Landing.tsx
@@ -1,19 +1,32 @@
 import React, { FC } from 'react'
-import { Stack, Button, Box, VStack, Center } from '@chakra-ui/react'
+import { Button, Box, VStack, Center, Flex } from '@chakra-ui/react'
+import { useHistory } from 'react-router-dom'
+
+import { useAuth } from '../contexts'
 
 export const Landing: FC = () => {
+  const history = useHistory()
+  const auth = useAuth()
+
+  const login = () => history.push('/login')
+
   return (
-    <VStack>
-      <Stack w="100vw" direction="row" spacing={2}>
-        <Box flex="1" />
-        <Button color="primary">Sign In</Button>
-      </Stack>
-      <Center h="100vh">
+    <Flex direction="column" height="100vh">
+      <Flex direction="row-reverse" w="100%" px={8} py={4}>
+        {auth.isAuthenticated ? (
+          <Button colorScheme="primary">Logout</Button>
+        ) : (
+          <Button onClick={login} colorScheme="primary">
+            Sign in
+          </Button>
+        )}
+      </Flex>
+      <Center flex="1">
         <VStack>
           <Box textStyle="h1">CheckFirst.gov.sg</Box>
           <Box>Don't Know? CheckFirst.</Box>
         </VStack>
       </Center>
-    </VStack>
+    </Flex>
   )
 }
