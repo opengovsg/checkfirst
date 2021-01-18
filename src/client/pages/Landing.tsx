6b23711a2a53ff369a647206282ea8b075ab8de0
LoneRifle <LoneRifle@users.noreply.github.com>
2021-01-18 10:12:07 +0800

    feat(signin): add a sign-in button to the homepage

--- src/client/pages/Landing.tsx
+++ src/client/pages/Landing.tsx
@@ -1,13 +1,19 @@
 import React, { FC } from 'react'
-import { Box, VStack, Center } from '@chakra-ui/react'
+import { Stack, Button, Box, VStack, Center } from '@chakra-ui/react'
 
 export const Landing: FC = () => {
   return (
-    <Center h="100vh">
-      <VStack>
-        <Box textStyle="h1">CheckFirst.gov.sg</Box>
-        <Box>Don't Know? CheckFirst.</Box>
-      </VStack>
-    </Center>
+    <VStack>
+      <Stack w="100vw" direction="row" spacing={2}>
+        <Box flex="1" />
+        <Button color="primary">Sign In</Button>
+      </Stack>
+      <Center h="100vh">
+        <VStack>
+          <Box textStyle="h1">CheckFirst.gov.sg</Box>
+          <Box>Don't Know? CheckFirst.</Box>
+        </VStack>
+      </Center>
+    </VStack>
   )
 }
