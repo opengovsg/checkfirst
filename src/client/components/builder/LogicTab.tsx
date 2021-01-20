a3117ba75f81fe15c535fec6769a8da9d3889682
Lam Kee Wei <keewei.lam@gmail.com>
2021-01-20 22:33:47 +0800

    refactor: split tabs in form builder into separate components

---
+++ src/client/components/builder/LogicTab.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+import { Container } from '@chakra-ui/react'
+
+export const LogicTab: FC = () => {
+  return (
+    <Container maxW="756px" px={0}>
+      Logic UI Components Here
+    </Container>
+  )
+}
