d94b909a5eef5f8815f0aaa37367a3f36ae83ac8
Lam Kee Wei <keewei.lam@gmail.com>
2021-01-20 11:56:33 +0800

    chore: add react hooks eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
