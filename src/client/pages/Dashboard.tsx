f3b97c19e9515c977e15daa36dd796916b24ebad
LoneRifle <LoneRifle@users.noreply.github.com>
2021-01-21 14:14:14 +0800

    feat(dashboard): retrieve user's checkers at dashboard
    On Dashboard mounting, retrieve the list of checkers from `/api/v1/c`
    and render them as cards onto the dashboard

--- src/client/pages/Dashboard.tsx
+++ src/client/pages/Dashboard.tsx
@@ -1,9 +1,22 @@
-import React, { FC } from 'react'
+import React, { useState, FC, useEffect } from 'react'
 import { Box, Container, Flex, Grid, VStack } from '@chakra-ui/react'
 
+import { ApiClient } from '../api'
 import { Navbar, CreateNew } from '../components/dashboard'
+import { Checker } from '../../types/checker'
 
 export const Dashboard: FC = () => {
+  const [checkers, setCheckers] = useState<Checker[]>([])
+
+  const loadCheckers = async () => {
+    const response = await ApiClient.get('/c')
+    setCheckers(response.data)
+  }
+
+  useEffect(() => {
+    loadCheckers()
+  }, [])
+
   return (
     <Flex direction="column" minH="100vh" bgColor="#F4F6F9">
       <Navbar />
@@ -11,14 +24,9 @@ export const Dashboard: FC = () => {
         <VStack align="stretch" py={10} position="relative">
           <Grid templateColumns="repeat(5, 1fr)" gap={6}>
             <CreateNew />
-            <Box w="100%" h="10" bg="blue.500" />
-            <Box w="100%" h="10" bg="blue.500" />
-            <Box w="100%" h="10" bg="blue.500" />
-            <Box w="100%" h="10" bg="blue.500" />
-            <Box w="100%" h="10" bg="blue.500" />
-            <Box w="100%" h="10" bg="blue.500" />
-            <Box w="100%" h="10" bg="blue.500" />
-            <Box w="100%" h="10" bg="blue.500" />
+            {checkers.map(() => {
+              return <Box w="100%" h="10" bg="blue.500" />
+            })}
           </Grid>
         </VStack>
       </Container>
