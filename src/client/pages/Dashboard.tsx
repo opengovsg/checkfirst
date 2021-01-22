6e98441b4881ab6a7c4b94d68b4bf84dd982a9c2
LoneRifle <LoneRifle@users.noreply.github.com>
2021-01-22 09:11:54 +0800

    refactor(dashboard): reduce boilerplate with react-query

--- src/client/pages/Dashboard.tsx
+++ src/client/pages/Dashboard.tsx
@@ -1,36 +1,37 @@
-import React, { useState, FC, useEffect } from 'react'
-import { Container, Flex, Grid, VStack } from '@chakra-ui/react'
+import React, { FC } from 'react'
+import { useQuery, useQueryClient } from 'react-query'
+import { Container, Flex, Grid, Spinner, VStack } from '@chakra-ui/react'
 
 import { ApiClient } from '../api'
 import { Navbar, CreateNew, CheckerCard } from '../components/dashboard'
 import { Checker } from '../../types/checker'
 
 export const Dashboard: FC = () => {
-  const [checkers, setCheckers] = useState<Checker[]>([])
-
-  const loadCheckers = async () => {
+  const { isLoading, data: checkers } = useQuery('checkers', async () => {
     const response = await ApiClient.get('/c')
-    setCheckers(response.data)
-  }
+    return (response.data || []) as Checker[]
+  })
+  const queryClient = useQueryClient()
 
-  useEffect(() => {
-    loadCheckers()
-  }, [])
+  const refetchCheckers = () => {
+    queryClient.refetchQueries('checkers')
+  }
 
   return (
     <Flex direction="column" minH="100vh" bgColor="#F4F6F9">
       <Navbar />
       <Container maxW="756px" pt="80px" px={0}>
         <VStack align="stretch" py={10} position="relative">
+          {isLoading && <Spinner />}
           <Grid templateColumns="repeat(5, 1fr)" gap={6}>
-            <CreateNew onSuccess={loadCheckers} />
-            {checkers.map((checker) => {
+            <CreateNew onSuccess={refetchCheckers} />
+            {checkers?.map((checker) => {
               return (
                 <CheckerCard
                   key={checker.id}
                   checker={checker}
-                  onDelete={loadCheckers}
-                  onDuplicate={loadCheckers}
+                  onDelete={refetchCheckers}
+                  onDuplicate={refetchCheckers}
                 />
               )
             })}
