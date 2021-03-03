bd5903d50e9259aa0194d6f6c86b83a553ef7c77
LoneRifle <LoneRifle@users.noreply.github.com>
2021-03-03 17:35:37 +0800

    feat(landing): add graphics, mobile responsiveness (#197)
    * feat(landing): add graphics, mobile responsiveness
    
    - insert graphics from wireframes (thanks @NatMaeTan)
    - rework mobile responsiveness so that the site is usable from
      smaller screens
    
    * feat(landing): add graphics, mobile responsiveness
    
    - insert graphics from wireframes (thanks @NatMaeTan)
    - rework mobile responsiveness so that the site is usable from
      smaller screens
    
    * style: update style to match Figma mobile design
    
    Co-authored-by: Lam Kee Wei <keewei.lam@gmail.com>

---
+++ src/client/pages/Landing/components/Faq.tsx
@@ -0,0 +1,71 @@
+import React, { FC } from 'react'
+import {
+  Box,
+  VStack,
+  Text,
+  useBreakpointValue,
+  OrderedList,
+  ListItem,
+} from '@chakra-ui/react'
+
+import { Section } from './Section'
+
+export const Faq: FC = () => {
+  const sectionHeader = useBreakpointValue({
+    base: 'h2',
+    lg: 'display2',
+  })
+  const infoHeader = useBreakpointValue({
+    base: 'h3',
+    lg: 'h2',
+  })
+
+  return (
+    <Section bg="#F4F6F9">
+      <VStack py="64px" align="stretch" spacing={{ base: '32px', md: '48px' }}>
+        <Text
+          textAlign={{ base: 'left', md: 'center' }}
+          textStyle={sectionHeader}
+        >
+          Frequently Asked Questions
+        </Text>
+
+        <VStack alignItems="left" justifyContent="center" spacing="16px">
+          <Text textStyle={infoHeader}>What can CheckFirst do?</Text>
+          <Text color="primary.500">
+            CheckFirst allows the user to build a custom eligibility checker,
+            calculator, or quiz easily.
+          </Text>
+        </VStack>
+
+        <VStack alignItems="left" justifyContent="center" spacing="16px">
+          <Text textStyle={infoHeader}>
+            What problems are CheckFirst solving?
+          </Text>
+          <Box>
+            <OrderedList spacing="16px">
+              <ListItem pl={{ base: '16px', md: '48px' }} color="primary.500">
+                <strong>Non-standard eligibility checkers:</strong> Some
+                agencies have Excel calculators while others use FAQs. From a
+                citizen’s perspective, he or she just wants to know his or her
+                eligiblity for a scheme.
+              </ListItem>
+
+              <ListItem pl={{ base: '16px', md: '48px' }} color="primary.500">
+                <strong>Long lead-time:</strong> Custom eligibility checkers
+                take time and money to build. User relies on an outside IT team
+                to build a customised solution that cannot scale or be reused.
+              </ListItem>
+
+              <ListItem pl={{ base: '16px', md: '48px' }} color="primary.500">
+                <strong>Tedious updates for multiple sites:</strong> The same
+                government scheme might have to be updated across multiple
+                websites. Manual errors can be introduced.
+              </ListItem>
+            </OrderedList>
+          </Box>
+        </VStack>
+      </VStack>
+    </Section>
+  )
+}
---
+++ src/client/pages/Landing/components/OgpFooter.tsx
@@ -0,0 +1,48 @@
+import React, { FC } from 'react'
+import { VStack, HStack, Link, Image, Text, Stack } from '@chakra-ui/react'
+import { ImLinkedin2, ImFacebook } from 'react-icons/im'
+
+import { Section } from './Section'
+import OGP from '../../../assets/ogp-logo.svg'
+
+export const OgpFooter: FC = () => (
+  <Section bg="neutral.100">
+    <Stack
+      py={{ base: '48px', md: '24px' }}
+      direction={{ base: 'column', md: 'row' }}
+      justifyContent={{ md: 'space-between' }}
+      alignItems={{ md: 'flex-end' }}
+      color="grey"
+      fontSize="10px"
+      spacing={{ base: '48px', md: '0px]' }}
+    >
+      <VStack
+        justifyContent="flex-start"
+        alignItems={{ base: 'flex-start', md: 'center' }}
+      >
+        <Text width="100%">Built by</Text>
+        <Link href="https://open.gov.sg" isExternal>
+          <Image htmlWidth="160px" htmlHeight="47px" src={OGP} />
+        </Link>
+      </VStack>
+
+      <VStack align="stretch">
+        <HStack justifyContent={{ md: 'flex-end' }}>
+          <Link
+            href="https://www.linkedin.com/company/open-government-products/"
+            isExternal
+          >
+            <ImLinkedin2 color="black" size="24px" />
+          </Link>
+          <Link href="https://www.facebook.com/opengovsg" isExternal>
+            <ImFacebook color="black" size="24px" />
+          </Link>
+        </HStack>
+        <Text>
+          © 2020 Open Government Products, Government Technology Agency of
+          Singapore
+        </Text>
+      </VStack>
+    </Stack>
+  </Section>
+)
