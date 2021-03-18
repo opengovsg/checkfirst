412b8645c8c15f8a6a580c95684a23369ef72b69
LoneRifle <LoneRifle@users.noreply.github.com>
2021-03-18 18:43:16 +0800

    build(webpack): use asset modules, drop file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = () => {
       path: path.join(__dirname, outputDirectory),
       filename: 'bundle.js',
       publicPath: '/',
+      assetModuleFilename: 'assets/[name].[ext]',
     },
     resolve: {
       extensions: ['.jsx', '.js', '.tsx', '.ts', '.json'],
@@ -41,15 +42,7 @@ module.exports = () => {
         },
         {
           test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                outputPath: 'assets',
-                name: '[name].[ext]',
-              },
-            },
-          ],
+          type: 'asset/resource',
         },
       ],
     },
