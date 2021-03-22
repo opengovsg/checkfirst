8237f19fc53ffe8f29321cb208fd2d77fb474814
barnettx <18087283+barnettx@users.noreply.github.com>
2021-03-22 12:22:19 +0800

    feat: add sentry react router integration
    - use history v4.10.1 because v5 is not compatible
    with react router v55
    - change from BrowserRouter to Router to access history

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,17 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import * as Sentry from '@sentry/react'
-import { Integrations } from '@sentry/tracing'
 import App from './App'
 
 import './styles/index.css'
-
-// maybe set a default if process.env.SENTRY_DSN is undefined
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  integrations: [new Integrations.BrowserTracing()],
-  tracesSampleRate: 1.0,
-})
 
 ReactDOM.render(
   <React.StrictMode>
