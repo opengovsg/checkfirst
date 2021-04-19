fce7fe4e93585589669e25757d80156638fb73a2
Lam Kee Wei <keewei.lam@gmail.com>
2021-04-19 11:47:46 +0800

    feat: add base colors and typography

--- src/client/theme/foundations/fonts.ts
+++ src/client/theme/foundations/fonts.ts
@@ -2,9 +2,9 @@ import { theme } from '@chakra-ui/react'
 
 export const fonts = {
   ...theme.fonts,
-  body: "'IBM Plex Sans', sans-serif",
+  body: "'Inter', sans-serif",
+  heading: "'Inter', sans-serif",
   ssp: "'Source Sans Pro', sans-serif",
-  ibm: "'IBM Plex Sans', sans-serif",
   mono: "'JetBrains Mono', monospace",
   inter: "'Inter', sans-serif",
 }
