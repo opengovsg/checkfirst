ad8809a81206d0987af0a928d35ae6fc2d243440
Lam Kee Wei <keewei.lam@gmail.com>
2021-04-19 11:47:46 +0800

    feat: add base colors and typography

---
+++ src/client/theme/foundations/fonts.ts
@@ -0,0 +1,8 @@
+import { theme } from '@chakra-ui/react'
+
+export const fonts = {
+  ...theme.fonts,
+  body: "'Inter', sans-serif",
+  heading: "'Inter', sans-serif",
+  mono: "'JetBrains Mono', monospace",
+}
