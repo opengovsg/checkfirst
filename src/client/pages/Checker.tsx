1de6c5902fac16fed39302fbabd9a9f996473f41
Preston Lim <prestonlimlianjie@gmail.com>
2021-04-20 21:27:10 +0800

    feat: add 404 error view

--- src/client/pages/Checker.tsx
+++ src/client/pages/Checker.tsx
@@ -1,10 +1,23 @@
 import React, { FC } from 'react'
 import { useQuery } from 'react-query'
 import { useParams } from 'react-router-dom'
-import { Center, Flex, Spinner } from '@chakra-ui/react'
+import {
+  Box,
+  Center,
+  Flex,
+  Spinner,
+  Image,
+  Heading,
+  Text,
+  VStack,
+} from '@chakra-ui/react'
 
 import { Checker as CheckerComponent } from '../components'
 import { CheckerService } from '../services'
+
+// Images
+import notFoundErrorImage from '../assets/states/error-404.svg'
+import Logo from '../assets/checkfirst-logo.svg'
 
 export const Checker: FC = () => {
   const { id } = useParams<{ id: string }>()
@@ -20,7 +33,28 @@ export const Checker: FC = () => {
           <Spinner size="xl" color="primary.500" thickness="4px" />
         </Center>
       )}
-      {isError && <Center py={16}>Oops an error has occurred!</Center>}
+      {isError && (
+        <Center py={16}>
+          <VStack spacing={4}>
+            <Image
+              flex={1}
+              src={notFoundErrorImage}
+              height={{ base: '257px', lg: 'auto' }}
+              mb={{ base: '24px', lg: '0px' }}
+            />
+            <Heading size="md" color="#1B3C87">
+              There’s nothing here.
+            </Heading>
+            <Text>
+              If you think this is a mistake, please contact the agency that
+              gave you the checker link.
+            </Text>
+            <Box pt={32}>
+              <Image htmlWidth="144px" src={Logo} />
+            </Box>
+          </VStack>
+        </Center>
+      )}
     </Flex>
   )
 }
