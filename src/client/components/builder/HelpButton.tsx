88b0d56098eb912f60fd3ce0f77c969343025491
Lam Kee Wei <keewei.lam@gmail.com>
2021-05-04 16:08:06 +0800

    chore(builder): add GA to help button (#488)

--- src/client/components/builder/HelpButton.tsx
+++ src/client/components/builder/HelpButton.tsx
@@ -1,19 +1,26 @@
-import { Circle, Flex, Link } from '@chakra-ui/react'
+import { OutboundLink } from 'react-ga'
+import { Circle, Flex } from '@chakra-ui/react'
 import React, { FC } from 'react'
 import { BiQuestionMark } from 'react-icons/bi'
+import { useGoogleAnalytics } from '../../contexts'
 import { DefaultTooltip } from '../common/DefaultTooltip'
 
 export const HelpButton: FC = () => {
+  const { GA_USER_EVENTS } = useGoogleAnalytics()
   return (
     <>
       <Flex position="fixed" left="40px" bottom="40px">
-        <Link href="https://guide.checkfirst.gov.sg" isExternal>
+        <OutboundLink
+          to="https://guide.checkfirst.gov.sg"
+          target="_blank"
+          eventLabel={GA_USER_EVENTS.BUILDER_HELP_BUTTON}
+        >
           <DefaultTooltip label="Help & Resources" placement="right">
             <Circle size="40px" bg="primary.500" color="white">
               <BiQuestionMark size="24px" />
             </Circle>
           </DefaultTooltip>
-        </Link>
+        </OutboundLink>
       </Flex>
     </>
   )
