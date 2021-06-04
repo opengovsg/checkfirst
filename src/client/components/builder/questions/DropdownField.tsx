9898d6f185aa39acb804e4a335c10210523adc87
Eugene Long <orbitalsqwib@gmail.com>
2021-06-04 13:06:59 +0800

    feat(dropdown-field): align to design system, extract styles to theme
    - updated text styles
    - updated and simplified layout

--- src/client/components/builder/questions/DropdownField.tsx
+++ src/client/components/builder/questions/DropdownField.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BiListUl } from 'react-icons/bi'
 import {
-  Box,
   HStack,
   VStack,
   Text,
@@ -9,6 +8,8 @@ import {
   Select,
   Textarea,
   useStyles,
+  InputGroup,
+  InputLeftElement,
 } from '@chakra-ui/react'
 
 import { useCheckerContext } from '../../../contexts'
@@ -18,6 +19,7 @@ import { BuilderActionEnum, ConfigArrayEnum } from '../../../../util/enums'
 const InputComponent: QuestionFieldComponent = ({ field, index }) => {
   const { title, description } = field
   const { dispatch } = useCheckerContext()
+  const commonStyles = useStyles()
 
   const updateTitleOrDescription = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target
@@ -46,52 +48,57 @@ const InputComponent: QuestionFieldComponent = ({ field, index }) => {
   }
 
   return (
-    <HStack w="100%" alignItems="flex-start">
-      <Box fontSize="20px" pt={2}>
-        <BiListUl />
-      </Box>
-      <VStack align="stretch" w="90%" spacing={6}>
-        <VStack align="stretch" spacing={2}>
-          <Input
-            type="text"
-            name="title"
-            placeholder="Question"
-            onChange={updateTitleOrDescription}
-            value={title}
-          />
-          <Input
-            type="text"
-            name="description"
-            placeholder="Description"
-            onChange={updateTitleOrDescription}
-            value={description}
-          />
-        </VStack>
-        <VStack spacing={4} alignItems="left" w="50%">
-          <Textarea
-            value={field.options.map((o) => o.label).join('\n')}
-            onChange={changeOptions}
-            placeholder="Enter each option on a new line"
-          />
-        </VStack>
+    <VStack sx={commonStyles.fullWidthContainer} spacing={4}>
+      <InputGroup>
+        <InputLeftElement
+          sx={commonStyles.inputIconElement}
+          children={<BiListUl />}
+        />
+        <Input
+          type="text"
+          sx={commonStyles.fieldInput}
+          name="title"
+          placeholder="Question"
+          onChange={updateTitleOrDescription}
+          value={title}
+        />
+      </InputGroup>
+      <Input
+        type="text"
+        sx={commonStyles.fieldInput}
+        name="description"
+        placeholder="Description"
+        onChange={updateTitleOrDescription}
+        value={description}
+      />
+      <VStack sx={commonStyles.halfWidthContainer} spacing={4}>
+        <Textarea
+          sx={commonStyles.fieldInput}
+          value={field.options.map((o) => o.label).join('\n')}
+          onChange={changeOptions}
+          placeholder="Enter each option on a new line"
+        />
       </VStack>
-    </HStack>
+    </VStack>
   )
 }
 
 const PreviewComponent: QuestionFieldComponent = ({ field }) => {
   const { title, description, options } = field
-  const styles = useStyles()
+  const commonStyles = useStyles()
+
   return (
-    <VStack align="stretch" w="100%" spacing={6}>
-      <VStack align="stretch">
+    <VStack sx={commonStyles.fullWidthContainer} spacing={3}>
+      <VStack sx={commonStyles.fullWidthContainer} spacing={0}>
         <HStack>
           <BiListUl fontSize="20px" />
-          <Text>{title}</Text>
+          <Text sx={commonStyles.previewTitle}>{title}</Text>
         </HStack>
-        {description && <Text color="secondary.400">{description}</Text>}
+        {description && (
+          <Text sx={commonStyles.previewDescription}>{description}</Text>
+        )}
       </VStack>
-      <Select isDisabled sx={styles.dummyInput}>
+      <Select isDisabled sx={commonStyles.dummyInput}>
         {options.map(({ value, label }, i) => (
           <option key={i} value={value}>
             {label}
