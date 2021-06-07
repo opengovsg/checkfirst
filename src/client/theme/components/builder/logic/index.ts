da689a3faedbb8357d47d8fe92333bc42af415b3
Eugene Long <orbitalsqwib@gmail.com>
2021-06-07 19:13:06 +0800

    feat(date-result-field): align to design system, extract styles
    - updated text styles
    - updated and simplified layout
    - added additional field-specific styles in a separate theme file

--- src/client/theme/components/builder/logic/index.ts
+++ src/client/theme/components/builder/logic/index.ts
@@ -1,7 +1,9 @@
 import { ConditionalResult } from './ConditionalResult'
 import { MapResult } from './MapResult'
+import { DateResult } from './DateResult'
 
 export const logic = {
   ConditionalResult,
   MapResult,
+  DateResult,
 }
