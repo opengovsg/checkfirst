3bd94f0fc069e21e32d0fc1724252e4b7ccbd90c
Kar Rui Lau <karrui@open.gov.sg>
2024-10-11 19:08:43 +0800

    feat: update jest packages

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '^.+.tsx?$': ['ts-jest', {}],
+  },
   moduleNameMapper: {
     '^.+\\.(css)$': '<rootDir>/config/css-stub.ts',
   },
